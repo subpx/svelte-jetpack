58663de67cdcefa2e94e9ef9d82e0987f5c8cc9f
Thomas Hynard <tomhynard@gmail.com>
2017-12-08 13:32:52 +1100

    Removing analyser

--- webpack.config/webpack.config.build.js
+++ webpack.config/webpack.config.build.js
@@ -1,11 +1,9 @@
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const baseConfig = require('./webpack.config.base');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const Config = Object.assign({}, baseConfig);
 
 Config.plugins = [
-  new BundleAnalyzerPlugin(),
   new UglifyJSPlugin(
     {
       uglifyOptions: {
