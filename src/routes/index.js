189b79e71354b0156578dd3139c87c60170a3c50
Thomas Hynard <tomhynard@gmail.com>
2017-12-11 12:50:03 +1100

    Include 404

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,96 +9,98 @@ import Sortable from '../modules/Shared/Sortable/Sortable.html';
 import DatePicker from '../modules/Shared/DatePicker/DatePicker.html';
 import AutoComplete from '../modules/Shared/AutoComplete/AutoComplete.html';
 import ToolTip from '../modules/Shared/ToolTip/ToolTip.html';
+import PageNotFound from '../modules/PageNotFound/PageNotFound.html';
 
-const routes = () => {
-  stateRouter.addState({
-    name: 'signIn',
-    route: '/signIn',
-    template: SignIn,
-    resolve(data, parameters, callback) {
-      auth.clear();
+stateRouter.addState({
+  name: 'pageNotFound',
+  route: '/pageNotFound',
+  template: PageNotFound
+});
+
+stateRouter.addState({
+  name: 'signIn',
+  route: '/signIn',
+  template: SignIn,
+  resolve(data, parameters, callback) {
+    auth.clear();
+    callback();
+  }
+});
+
+stateRouter.addState({
+  name: 'app',
+  route: '/app',
+  template: App,
+  resolve(data, parameters, callback) {
+    if (auth.getToken()) {
       callback();
-    },
-  });
-
-  stateRouter.addState({
-    name: 'app',
-    route: '/app',
-    template: App,
-    resolve(data, parameters, callback) {
-      if (auth.getToken()) {
-        callback();
-      } else {
-        callback.redirect('signIn');
-      }
-    },
-  });
-
-  stateRouter.addState({
-    name: 'app.dashboard',
-    route: '/dashboard',
-    template: Dashboard
-  });
-
-  stateRouter.addState({
-    name: 'app.dashboard.child',
-    route: '/child',
-    template: Child
-  });
-
-  stateRouter.addState({
-    name: 'app.formValidation',
-    route: '/formValidation',
-    template: FormValidation
-  });
-
-  stateRouter.addState({
-    name: 'app.sortable',
-    route: '/sortable',
-    template: Sortable
-  });
-
-  stateRouter.addState({
-    name: 'app.datePicker',
-    route: '/datePicker',
-    template: DatePicker
-  });
-
-  stateRouter.addState({
-    name: 'app.autoComplete',
-    route: '/autoComplete',
-    template: AutoComplete
-  });
-
-  stateRouter.addState({
-    name: 'app.toolTip',
-    route: '/toolTip',
-    template: ToolTip
-  });
-
-  stateRouter.on('routeNotFound', (route) => {
-    if(route === '/') {
-      stateRouter.go('signIn');
+    } else {
+      callback.redirect('signIn');
     }
-    // stateRouter.go('not-found', {
-    //   route: route,
-    //   parameters: parameters
-    // })
-  });
+  }
+});
 
-  stateRouter.on('stateChangeStart', (route, parameters) => {
-    console.log('stateChangeStart', route, parameters);
-  });
+stateRouter.addState({
+  name: 'app.dashboard',
+  route: '/dashboard',
+  template: Dashboard
+});
 
-  stateRouter.on('stateChangeEnd', (route, parameters) => {
-    console.log('stateChangeEnd', route, parameters);
-  });
+stateRouter.addState({
+  name: 'app.dashboard.child',
+  route: '/child',
+  template: Child
+});
 
-  stateRouter.on('stateError', (route, parameters) => {
-    console.log('stateError', route, parameters);
-  });
+stateRouter.addState({
+  name: 'app.formValidation',
+  route: '/formValidation',
+  template: FormValidation
+});
 
-  stateRouter.evaluateCurrentRoute('signIn');
-};
+stateRouter.addState({
+  name: 'app.sortable',
+  route: '/sortable',
+  template: Sortable
+});
 
-export default routes;
+stateRouter.addState({
+  name: 'app.datePicker',
+  route: '/datePicker',
+  template: DatePicker
+});
+
+stateRouter.addState({
+  name: 'app.autoComplete',
+  route: '/autoComplete',
+  template: AutoComplete
+});
+
+stateRouter.addState({
+  name: 'app.toolTip',
+  route: '/toolTip',
+  template: ToolTip
+});
+
+stateRouter.on('routeNotFound', (route) => {
+  if(route === '/') {
+    stateRouter.go('signIn');
+  } else {
+    stateRouter.go('pageNotFound');
+  }
+});
+
+stateRouter.on('stateChangeStart', (route, parameters) => {
+  console.log('stateChangeStart', route, parameters);
+});
+
+stateRouter.on('stateChangeEnd', (route, parameters) => {
+  console.log('stateChangeEnd', route, parameters);
+});
+
+stateRouter.on('stateError', (route, parameters) => {
+  console.log('stateError', route, parameters);
+});
+
+stateRouter.evaluateCurrentRoute('signIn');
+
--- src/services/router.js
+++ src/services/router.js
@@ -4,4 +4,9 @@ import makeSvelteStateRenderer from 'svelte-state-renderer';
 const renderer = makeSvelteStateRenderer();
 const stateRouter = StateRouter(renderer, document.querySelector('main'));
 
+// Store current route and params?
+stateRouter.on('stateChangeEnd', (route, parameters) => {
+  console.log(route, parameters);
+});
+
 export default stateRouter;
