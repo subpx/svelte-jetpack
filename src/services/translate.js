e529d021a27dfd099452aadb99208fa888749df2
Thomas Hynard <tomhynard@gmail.com>
2017-12-11 21:13:41 +1100

    Better translate defaults

--- src/services/translate.js
+++ src/services/translate.js
@@ -1,11 +1,10 @@
 import i18next from 'i18next';
 
 const translate = i18next.createInstance({
-  whitelist: ['en', 'fr'],
   fallbackLng: 'en',
-  debug: true,
+  defaultNS: 'common'
 }, () => {
-  console.log('we ready to translate');
+
 });
 
 export default translate;
