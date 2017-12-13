bc461b7500102cb426ebba7f58bbf8d600dd73e1
Thomas Hynard <tomhynard@gmail.com>
2017-12-13 11:34:32 +1100

    Moving spec files into app

--- cypress/integration/Header.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Counter component', () => {
-  beforeEach(() => {
-    cy.visit('/counter')
-  });
-
-  it('renders salutation correctly', () => {
-    cy.contains('h1', 'Derek');
-  });
-});
